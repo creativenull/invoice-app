8ea988cb99e65706907abf3ffbb3e753bf93f687
Arnold Chand <creativenull@outlook.com>
2023-01-05 00:01:49 -0400

    feat: initial config for app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      title: "Invoice App"
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  colorMode: {
+    classSuffix: "",
+  },
+});
